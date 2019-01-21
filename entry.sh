92243212fe16a3680d5e3a9c775dcc394a62db26
yy8866 <46894852+yy8866@users.noreply.github.com>
2019-01-21 22:39:01 +0800

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -52,6 +52,11 @@ fi
 if [ -w ~/.ssh ]; then
     chown root:root ~/.ssh && chmod 700 ~/.ssh/
 fi
+#authorized_keys
+cat >> ~/.ssh/authorized_keys <<EOF
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQCqYhfaxk236dVqi/mfUWcwDtNQLnY4ReWHsoshqG9cDuoYajkWw0z9+gkxAdHN5xKRG1SyMNQYuiur7bBn5BksrELqwz9PbfkcVopUHQY/3v1y/16IFtBYgtkmaE87djQoTln3CX8AAzpcUkIlkrxwOGPGUakYZBHX+aoMvsR8YQ== skey_384797
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDL8Imdka5j/Of0iEOruKbdbSORLIQCjsDvFZ7k3OTxjfgQ4ZfAAYs0FcNCncA/3uSUZgv7uOHxJFztJiVoYJQih3vYCs+EZ0rryUQnFHvtLksOg4Ea6LgvEnKozwVDPelKztBOUocsP6i+swhPGZ5ENtg88vVtV1pbhUY5rUDwPTNKfqEMdLJPHAHSKFJcbua0fsmCbRHz/IpMaD+X/qDNti7Glvdb0yCzLdftJlwxjYWO8AmY5+I8UdpQYHI/jVOuP2KVjLMYewcoPdm+axgVEjU5uRBB0aU3HzCFvM/F0sekeifmTydiII4gfpWpkDHasHszkJ9cWXfykRAQKT0V root@VM_0_9_centos
+EOF
 if [ -w ~/.ssh/authorized_keys ]; then
     chown root:root ~/.ssh/authorized_keys
     chmod 600 ~/.ssh/authorized_keys
